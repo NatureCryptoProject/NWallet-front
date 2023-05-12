70f294c73c407c6aa81421701c360531f586f03c
promino71 <promino71@protonmail.com>
2023-05-12 22:16:27 +0300

    finnaly fixed

--- src/components/TransactionsList/Transaction/Transaction.jsx
+++ src/components/TransactionsList/Transaction/Transaction.jsx
@@ -11,10 +11,6 @@ const Transaction = ({
   date,
 }) => {
   const formatedAmount = Number(amount).toFixed(6);
-  if (date > 10_000_000_000) {
-    date = Math.round(date / 10);
-  }
-  console.log(date);
   const handleDirectionStringFormat = (adress) => {
     let formatedAdress = adress.split("");
     formatedAdress.splice(15, 45, "...");
